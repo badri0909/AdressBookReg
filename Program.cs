62b2a549602c81526cbf5ef912d03f33e55c746d
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-16 10:46:11 +0530

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AddressBookReg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.Equals(null);
+        }
+    }
+}
