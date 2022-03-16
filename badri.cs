9b9f71979451f3c1203847a1257889c56ac2f71f
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-16 10:48:32 +0530

    add badri.cs

---
+++ badri.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace CollectionDemo
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Welcome to AddressBook Program");
+
+
+//            //Console.WriteLine("Enter your 1st Name:");
+//            //String Name = Console.ReadLine();
+//            //Console.WriteLine("Enter your last Name:");
+//            //String LastName = Console.ReadLine();           
+//            //Console.WriteLine("Enter your Address:");
+//            //String Address = Console.ReadLine();
+//            //Console.WriteLine("Enter your City:");
+//            //String City = Console.ReadLine();
+//            //Console.WriteLine("Enter your Zip Code:");
+//            //String Code = Console.ReadLine();
+//            //Console.WriteLine("Enter your Phone Number:");
+//            //String PhNo= Console.ReadLine();
+//            //Console.WriteLine("Enter your Email:");
+//            //String Email = Console.ReadLine();
+
+//            //Console.WriteLine("My full name is :" + Name + "\nLast Name is: " + LastName + "\nMy Address is: " + Address + "\nMy City Name : " + City + "\nMy Postal Code is : " + Code + "\nMy Contact Number is : " + PhNo + "\nMy Email Address is : " + Email );
+
+//            //doListDemo();
+//            // doStackDemo();
+//            // doQueueDemo();
+//            // doSetDemo();
+//            //doDictonaryDemo();
+//        }
+
+//    }
+//}
